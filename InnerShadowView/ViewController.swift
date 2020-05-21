7e8b11eccf9d1c5af1990bb7ca9cb879ab2cd259
Jeffrey Blagdon <jeffblagdon@gmail.com>
2020-05-21 15:53:44 -0300

    Initial Commit

---
+++ InnerShadowView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InnerShadowView
+//
+//  Created by Jeffrey Blagdon on 2020-05-21.
+//  Copyright © 2020 polyergy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
